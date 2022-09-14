4ef3bd81a94227756a10ca3a57b64a850074c2ae
CarlosJunioor <carlosjunior866@gmail.com>
2022-09-14 05:56:06 +0100

    treta

--- desafiotreta.c
+++ desafiotreta.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 #include<unistd.h>
 
+char ft_strlen(char *str)
+{
+    int i;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] < 65 || str[i] > 122)
+        {
+            printf("found it!");
+            
+        }
+        write(1,&str[i],1);
+        i++;
+    }
+    return 0;
+}
+
 int main (void)
 {
-     char t1;
+    char treta[] = "Sênior de 2 anos";
+    char date[] = "2022/07/09";
+    char string;
 
-    t1 = 34;
-    write(1,"teste"&t1,1);
-    write(1,"teste"&t1,1);
-    write(1,"teste"&t1,1);
-    
+    string = 136;
 
+    ft_strlen(treta);
     return 0;
 }
